5de80511736c511d0fe1f27f1112248d85c4688f
Anton Romanovsky <romcky@gmail.com>
2024-01-22 11:18:31 +0300

    Step 3 Add picocli and -hV option to the App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.5")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+    description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @Override
+    public Integer call() {
         System.out.println("Hello World!");
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
