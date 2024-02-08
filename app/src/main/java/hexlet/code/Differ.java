c70e57710dc76a9fe0497b9325f71b17b5b59bcf
Anton Romanovsky <romcky@gmail.com>
2024-02-08 11:59:58 +0300

    Fix FilePath

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,23 +7,11 @@ public class Differ {
 
     public static String generate(String filePath1, String filePath2)
             throws Exception {
-        return generate(filePath1, filePath1, "stylish");
+        return generate(filePath1, filePath2, "stylish");
     }
 
     public static String generate(String filePath1, String filePath2, String formatType)
             throws Exception {
-
-        //error in hexlet, so we do !!!!!!!!
-        /*
-        filePath2 = "";
-        for (int i = 0; i < filePath1.length(); i++) {
-            if (filePath1.charAt(i) != '1') {
-                filePath2 += filePath1.charAt(i);
-            } else {
-                filePath2 += '2';
-            }
-        }
-         */
 
         var fileData1 = new String(Files.readAllBytes(
                 Paths.get(filePath1).toAbsolutePath().normalize()));
@@ -37,10 +25,6 @@ public class Differ {
         var fileMap2 = Parser.parse(fileData2, fileType2);
 
         var diffList = Difference.getDifferenceList(fileMap1, fileMap2);
-
-        //error in hexlet test !!! generate(file1, file2) where file1 == file2 !!!
-        System.out.println("debug: file1: " + filePath1);
-        System.out.println("debug: file2: " + filePath2);
 
         return Formatter.format(diffList, formatType);
     }
